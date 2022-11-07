95cd4a630b61370c3c46d5535552f744808a72f5
snvjaos <thffntus3218@gmail.com>
2022-11-08 08:46:23 +0900

    github

---
+++ 04.JS학습/module/msgFormat.js
@@ -0,0 +1,12 @@
+// 구체적인 데이터 구성처리를 위한 JS - msgFormat.js
+
+// 내용 메시지 구성 함수
+const message = (name,age) => 
+    `나의 이름은 ${name}입니다. 
+    나이는 ${age}살입니다. 반갑습니다!!!^^<br>`;
+
+// 단일한 함수 내보내기!
+// export default message;
+export {message};
+// 만약 import에서 별칭을 쓰고 싶다면 default로 내보낼 수 없다!
+// default빼고 중괄호{} 로 묶어서 내보내야함!!!
